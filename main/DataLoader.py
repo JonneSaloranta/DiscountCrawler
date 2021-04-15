fdcd0a8af0e1b25a0e9210312a1036da6e92248b
JonSal97 <saloranta.jonne@gmail.com>
2021-04-15 22:17:40 +0300

    Added data saving to a file

--- main/DataLoader.py
+++ main/DataLoader.py
@@ -1,54 +1,118 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import CustomExceptions
+
+# -*- coding: utf-8 -*-
+
+path = "./"
+fileName = "data"
+
+data = {}
+data["Products"] = []
+
+ce = CustomExceptions
 
 
-class Sales:
+class Discounts:
 
     itemCount = 0
     productList = []
     priceList = []
 
     def __init__(self):
-        """Sales class init"""
-        print("Sales init")
+        """Discount class init"""
+        print("Discount init")
+
+    @staticmethod
+    def WriteToJsonFile(path, fileName, data):
+        """Writes data into json file
+
+        Args:
+            path (str): Filepath
+            fileName (str): Json file name
+            data (str): Data to be dumped into json file
+        """
+
+        filePathNameWithExt = "./" + path + "/" + fileName + ".json"
+        content = None
+        with open(filePathNameWithExt, "r", encoding="utf-8") as of:
+            content = json.load(of)
+
+        if content != data:
+            print("Saved new data!")
+            with open(filePathNameWithExt, "w", encoding="utf-8") as fp:
+                json.dump(data, fp, sort_keys=True, indent=4, ensure_ascii=False)
+        else:
+            print("New data is the same as the old data!")
+
+    @staticmethod
+    def ReadJsonData(path, fileName):
+        """Reads data from json file
+
+        Args:
+            path (str): Filepath
+            fileName (str): File name for file to be read
+        """
+        filePathNameWithExt = "./" + path + "/" + fileName + ".json"
+        with open(filePathNameWithExt) as of:
+            content = json.load(of)
+
+    def parseData(self, bs4set):
+        """Parses data in to an array.
+
+        Args:
+            bs4set (bs4.element.ResultSet): Type must be bs4.element.ResultSet
+
+        Raises:
+            ValueError: Raises ValueError if bs4set type is not bs4.element.ResultSet
+            ce.ProductTitleException: Raises ProductTitleException if title is not a string
+            ce.ProductPriceException: Raises ProductPriceException if title is not a string
+        """
+        if isinstance(bs4set, type("bs4.element.ResultSet")):
+            raise ValueError("File type must be bs4.element.ResultSet")
+
+        for product in bs4set:
+            try:
+                src_product_title = product.find("h3", class_="product__title").text
+                src_product_price = product.find("span", class_="pricebox__price").text
+
+                if type(src_product_title) is not str:
+                    raise ce.ProductTitleException
+
+                if type(src_product_price) is not str:
+                    raise ce.ProductPriceException
+
+                product_title = " ".join(src_product_title.split())
+                product_price = " ".join(src_product_price.split())
+
+                self.productList.append(product_title)
+                self.priceList.append(product_price)
+
+                self.itemCount += 1
+
+                temp = {"name": product_title, "price": product_price}
+
+                data["Products"].append(temp)
+            except Exception as e:
+                print(e)
 
     @property
     def getData(self):
-        """Load sales data from lidl website"""
-        self.productList = []
-        self.priceList = []
-        self.itemCount = 0
+        """""Load discount data from lidl website""" ""
         src = requests.get("https://www.lidl.fi/tarjoukset").text
 
         soup = BeautifulSoup(src, "lxml")
 
         products = soup.find_all("article", class_="product product--tile")
 
-        for product in products:
-
-            try:
-                src_product_title = product.find("h3", class_="product__title").text
-                product_title = " ".join(src_product_title.split())
-                self.productList.append(product_title)
-
-            except Exception:
-                product_title = "Null"
-                self.productList.append(product_title)
-
-            try:
-                src_product_price = product.find("span", class_="pricebox__price").text
-                product_price = " ".join(src_product_price.split())
-                self.priceList.append(product_price)
-
-            except Exception:
-                product_price = "Null"
-                self.priceList.append(product_price)
-
-            self.itemCount += 1
+        self.parseData(products)
+        self.WriteToJsonFile(path, fileName, data)
+        self.ReadJsonData(path, fileName)
 
     @property
     def getItemCount(self):
-        """Returns sales item count
+        """Returns discount item count
 
         Returns:
             int: itemCount
@@ -56,7 +120,7 @@ class Sales:
         return self.itemCount
 
     def getItemName(self, number):
-        """Get item from sale item list
+        """Get item from discount item list
 
         Args:
             number (int): n item from the list
@@ -69,18 +133,22 @@ class Sales:
     def getItemPrice(self, number):
         return self.priceList[number]
 
-    @property
-    def displayAllItems(self):
-        """Returns with all of the items in a sales list"""
-        for (item, price) in zip(self.productList, self.priceList):
-            print(str(item + " - " + price))
+    def displayAllItems(self, path, fileName):
+        content = None
+
+        filePathNameWithExt = "./" + path + "/" + fileName + ".json"
+        with open(filePathNameWithExt) as of:
+            content = json.load(of)
+
+        for i in content["Products"]:
+            print(i["name"], i["price"])
 
 
 def _test():
-    sales = Sales()
-    sales.getData
-    print(sales.getItemCount)
-    sales.displayAllItems
+    disc = Discounts()
+    disc.getData
+    print(disc.getItemCount)
+    disc.displayAllItems(path, fileName)
 
 
 if __name__ == "__main__":
